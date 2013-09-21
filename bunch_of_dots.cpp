a86075327594eca015e0fddeef140a01abcfa34d
Mikhail Bulash <scaryspiderpig@gmail.com>
2013-09-21 22:24:27 +0300

    initial commit

---
+++ bunch_of_dots.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "SDL2/SDL.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+    atexit(SDL_Quit);
+
+    SDL_Window *screen = SDL_CreateWindow("Bunch of dots v0.1",
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
+        0, 0,
+        SDL_WINDOW_FULLSCREEN_DESKTOP
+    );
+    SDL_Renderer *renderer = SDL_CreateRenderer(screen, -1,
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+    SDL_SetRenderDrawColor(renderer, 127, 127, 255, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_Delay(10000);
+    return 0;
+}
+
